265b262d8f584ae88ee2c178a4094038a16619f1
pete <ersts@amnh.org>
2020-02-05 14:54:01 -0500

    compiled based on older version for compat.

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-javac -cp ../lib/Jama-1.0.2.jar:. -d ../bin GUI/*.java
-javac -cp ../lib/Jama-1.0.2.jar:. -d ../bin DataProvider/*.java
-javac -cp ../lib/Jama-1.0.2.jar:. -d ../bin Imagery/*.java
-javac -cp ../lib/Jama-1.0.2.jar:. -d ../bin launcher.java
+javac -source 1.7 -target 1.7 -cp ../lib/Jama-1.0.2.jar:. -d ../bin GUI/*.java
+javac -source 1.7 -target 1.7 -cp ../lib/Jama-1.0.2.jar:. -d ../bin DataProvider/*.java
+javac -source 1.7 -target 1.7 -cp ../lib/Jama-1.0.2.jar:. -d ../bin Imagery/*.java
+javac -source 1.7 -target 1.7 -cp ../lib/Jama-1.0.2.jar:. -d ../bin launcher.java
 cp 7band_256x256_example.dat ../bin
 cp ../lib/Jama-1.0.2.jar ../bin
 cp cbc-blue-sm.jpg ../bin
